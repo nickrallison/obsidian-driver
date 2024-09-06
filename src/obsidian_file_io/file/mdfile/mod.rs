95c04c9f2856a6f460a0d303c81382552f207b7f
nickrallison <nickrallison1@gmail.com>
2024-09-06 09:32:23 -0600

    added mdfile tets

--- src/obsidian_file_io/file/mdfile/mod.rs
+++ src/obsidian_file_io/file/mdfile/mod.rs
@@ -13,11 +13,6 @@ pub(crate) struct MDFile {
 impl MDFile {
 	pub(crate) fn from_string(contents: String) -> Self {
         let yaml_pattern = Regex::new(r"^(\-\-\-\n(?P<yaml>[\s\S]*?)\n?\-\-\-\n?)?(?P<body>[\s\S]*)").unwrap();
-        // if yaml does capture, use the yaml capture group as the yaml value
-        // otherwise, set yaml to None
-
-        // if body does capture, use the body capture group as the body value
-        // otherwise, set body to ""
         let captures = yaml_pattern.captures(&contents).unwrap();
         let yaml = captures.name("yaml").map(|m| serde_yaml::from_str(m.as_str()).unwrap());
         let body = captures.name("body").unwrap().as_str().to_string();
@@ -47,7 +42,7 @@ mod mdfile_tests {
         use super::*;
     
         #[test]
-        fn test_from_string() {
+        fn test_from_string_with_yaml() {
             let contents = "---\nkey: value\n---\n# Test\n\nThis is a test file.".to_string();
 
             let actual = MDFile::from_string(contents);
@@ -62,7 +57,7 @@ mod mdfile_tests {
         }
     
         #[test]
-        fn test_to_string() {
+        fn test_to_string_with_yaml() {
             let mut yaml_expected: serde_yaml::Mapping = serde_yaml::Mapping::new();
             yaml_expected.insert(serde_yaml::Value::String("key".to_string()), serde_yaml::Value::String("value".to_string()));
             let mdfile = MDFile {
@@ -75,6 +70,33 @@ mod mdfile_tests {
 key: value
 ---
 # Test
+
+This is a test file."#.to_string();
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn test_from_string_without_yaml() {
+            let contents = "# Test\n\nThis is a test file.".to_string();
+
+            let actual = MDFile::from_string(contents);
+            let expected = MDFile {
+                yaml: None,
+                body: "# Test\n\nThis is a test file.".to_string(),
+                embedding: None,
+            };
+            assert_eq!(actual, expected);
+        }
+    
+        #[test]
+        fn test_to_string_without_yaml() {
+            let mdfile = MDFile {
+                yaml: None,
+                body: "# Test\n\nThis is a test file.".to_string(),
+                embedding: None,
+            };
+            let actual = mdfile.to_string();
+            let expected = r#"# Test
 
 This is a test file."#.to_string();
             assert_eq!(actual, expected);
