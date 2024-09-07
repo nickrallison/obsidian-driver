7204a7082a3bc1d024e085c3dc7c758c693861d8
nickrallison <nickrallison1@gmail.com>
2024-09-07 13:02:51 -0600

    got embedding working in public api

--- src/ai/api/openai.rs
+++ src/ai/api/openai.rs
@@ -1,171 +1,185 @@
-use std::path::PathBuf;
-use serde::{Deserialize, Serialize};
 use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
 
 use crate::prelude::*;
 
 #[derive(Clone, Debug)]
 pub struct OpenAIDriver {
-	config: OpenAIConfig,
-	client: Client,
+    config: OpenAIConfig,
+    client: Client,
 }
 
 impl OpenAIDriver {
-	pub fn new(config: OpenAIConfig) -> OpenAIDriver {
-		config.validate();
-		OpenAIDriver {
-			config,
-			client: Client::new(),
-		}
-	}
+    pub fn new(config: OpenAIConfig) -> OpenAIDriver {
+        config.validate();
+        OpenAIDriver {
+            config,
+            client: Client::new(),
+        }
+    }
 
-	pub async fn get_embedding(&self, text: &str) -> Result<String> {
-		let request_body = serde_json::json!({
+    pub async fn get_embedding(&self, text: &str) -> Result<Vec<f64>> {
+        let request_body = serde_json::json!({
             "input": text,
             "model": &self.config.embedding_model,
         });
 
-		let response = self.client.post(&self.config.embedding_url)
-			.header("Content-Type", "application/json")
-			.header("Authorization", format!("Bearer {}", self.config.api_key))
-			.json(&request_body)
-			.send()
-			.await?;
+        let response = self
+            .client
+            .post(&self.config.embedding_url)
+            .header("Content-Type", "application/json")
+            .header("Authorization", format!("Bearer {}", self.config.api_key))
+            .json(&request_body)
+            .send()
+            .await?;
 
-		let response_text = response.text().await?;
-		Ok(response_text)
-	}
-	pub async fn chat_smart(&self, prompt: crate::ai::prompt::Prompt) -> Result<String> {
-		let tokens = prompt.max_characters / self.config.characters_per_token;
-		if tokens > self.config.smart_model_max_tokens {
-			return Err(Error::PromptExceedsModelTokenLimit(prompt));
-		}
-		let request_body = serde_json::json!({
-			"model": &self.config.smart_text_model,
-			"messages": vec![
-				ChatMessage {
-					role: "system".to_string(),
-					content: prompt.system_prompt,
-				},
-				ChatMessage {
-					role: "user".to_string(),
-					content: prompt.user_prompt,
-				},
-			],
-			"max_tokens": tokens,
-		});
+        let response_text = response.text().await?;
+        let response_json: serde_json::Value = serde_json::from_str(&response_text)?;
+        let vec = response_json["data"][0]["embedding"]
+            .as_array()
+            .ok_or(Error::InvalidEmbeddingResponse(response_text))?
+            .iter()
+            .map(|v| v.as_f64().unwrap())
+            .collect();
+        Ok(vec)
+    }
 
-		let response = self.client.post(&self.config.chat_url)
-			.header("Content-Type", "application/json")
-			.header("Authorization", format!("Bearer {}", self.config.api_key))
-			.json(&request_body)
-			.send()
-			.await?;
+    pub async fn chat_smart(&self, prompt: crate::ai::prompt::Prompt) -> Result<String> {
+        let tokens = prompt.max_characters / self.config.characters_per_token;
+        if tokens > self.config.smart_model_max_tokens {
+            return Err(Error::PromptExceedsModelTokenLimit(prompt));
+        }
+        let request_body = serde_json::json!({
+            "model": &self.config.smart_text_model,
+            "messages": vec![
+                ChatMessage {
+                    role: "system".to_string(),
+                    content: prompt.system_prompt,
+                },
+                ChatMessage {
+                    role: "user".to_string(),
+                    content: prompt.user_prompt,
+                },
+            ],
+            "max_tokens": tokens,
+        });
 
-		let response_text = response.text().await?;
-		Ok(response_text)
-	}
+        let response = self
+            .client
+            .post(&self.config.chat_url)
+            .header("Content-Type", "application/json")
+            .header("Authorization", format!("Bearer {}", self.config.api_key))
+            .json(&request_body)
+            .send()
+            .await?;
 
-	pub async fn chat_cheap(&self, prompt: crate::ai::prompt::Prompt) -> Result<String> {
-		let tokens = prompt.max_characters / self.config.characters_per_token;
-		if tokens > self.config.cheap_model_max_tokens {
-			return Err(Error::PromptExceedsModelTokenLimit(prompt));
-		}
-		let request_body = serde_json::json!({
-			"model": &self.config.cheap_text_model,
-			"messages": vec![
-				ChatMessage {
-					role: "system".to_string(),
-					content: prompt.system_prompt,
-				},
-				ChatMessage {
-					role: "user".to_string(),
-					content: prompt.user_prompt,
-				},
-			],
-			"max_tokens": tokens,
-		});
+        let response_text = response.text().await?;
+        Ok(response_text)
+    }
 
-		let response = self.client.post(&self.config.chat_url)
-			.header("Content-Type", "application/json")
-			.header("Authorization", format!("Bearer {}", self.config.api_key))
-			.json(&request_body)
-			.send()
-			.await?;
+    pub async fn chat_cheap(&self, prompt: crate::ai::prompt::Prompt) -> Result<String> {
+        let tokens = prompt.max_characters / self.config.characters_per_token;
+        if tokens > self.config.cheap_model_max_tokens {
+            return Err(Error::PromptExceedsModelTokenLimit(prompt));
+        }
+        let request_body = serde_json::json!({
+            "model": &self.config.cheap_text_model,
+            "messages": vec![
+                ChatMessage {
+                    role: "system".to_string(),
+                    content: prompt.system_prompt,
+                },
+                ChatMessage {
+                    role: "user".to_string(),
+                    content: prompt.user_prompt,
+                },
+            ],
+            "max_tokens": tokens,
+        });
 
-		let response_text = response.text().await?;
-		Ok(response_text)
-	}
+        let response = self
+            .client
+            .post(&self.config.chat_url)
+            .header("Content-Type", "application/json")
+            .header("Authorization", format!("Bearer {}", self.config.api_key))
+            .json(&request_body)
+            .send()
+            .await?;
+
+        let response_text = response.text().await?;
+        Ok(response_text)
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct OpenAIConfig {
-	// Validation
-	pub validation_url: String,
+    // Validation
+    pub validation_url: String,
 
-	// Models
-	pub embedding_model: String,
-	pub smart_text_model: String,
-	pub cheap_text_model: String,
+    // Models
+    pub embedding_model: String,
+    pub smart_text_model: String,
+    pub cheap_text_model: String,
 
-	pub smart_model_max_tokens: u32,
-	pub cheap_model_max_tokens: u32,
+    pub smart_model_max_tokens: u32,
+    pub cheap_model_max_tokens: u32,
 
-	// Urls
-	pub embedding_url: String,
-	pub chat_url: String,
+    // Urls
+    pub embedding_url: String,
+    pub chat_url: String,
 
-	// API key
-	pub api_key: String,
+    // API key
+    pub api_key: String,
 
-	// Other
-	pub characters_per_token: u32,
-
+    // Other
+    pub characters_per_token: u32,
 }
 
 impl OpenAIConfig {
-	// Example curl command to list models, used as a validation step
-	// curl https://api.openai.com/v1/models \
-	//   -H "Authorization: Bearer $OPENAI_API_KEY" \
-	pub async fn validate(&self) -> Result<()> {
-		let validator = OpenAIValidator::new(self.clone());
-		validator.validate().await
-	}
+    // Example curl command to list models, used as a validation step
+    // curl https://api.openai.com/v1/models \
+    //   -H "Authorization: Bearer $OPENAI_API_KEY" \
+    pub async fn validate(&self) -> Result<()> {
+        let validator = OpenAIValidator::new(self.clone());
+        validator.validate().await
+    }
 
-	pub(crate) fn from_file(config_path: PathBuf) -> OpenAIConfig {
-		let config_file = std::fs::File::open(config_path).unwrap();
-		let config: OpenAIConfig = serde_json::from_reader(config_file).unwrap();
-		config
-	}
+    pub(crate) fn from_file(config_path: PathBuf) -> OpenAIConfig {
+        let config_file = std::fs::File::open(config_path).unwrap();
+        let config: OpenAIConfig = serde_json::from_reader(config_file).unwrap();
+        config
+    }
 }
 
 pub struct OpenAIValidator {
-	config: OpenAIConfig,
-	client: Client,
+    config: OpenAIConfig,
+    client: Client,
 }
 
 impl OpenAIValidator {
-	pub fn new(config: OpenAIConfig) -> OpenAIValidator {
-		OpenAIValidator {
-			config,
-			client: Client::new(),
-		}
-	}
+    pub fn new(config: OpenAIConfig) -> OpenAIValidator {
+        OpenAIValidator {
+            config,
+            client: Client::new(),
+        }
+    }
 
-	pub async fn validate(&self) -> Result<()> {
-		let response = self.client.get(&self.config.validation_url)
-			.header("Authorization", format!("Bearer {}", self.config.api_key))
-			.send()
-			.await?;
+    pub async fn validate(&self) -> Result<()> {
+        let response = self
+            .client
+            .get(&self.config.validation_url)
+            .header("Authorization", format!("Bearer {}", self.config.api_key))
+            .send()
+            .await?;
 
-		let response_text = response.text().await?;
-		panic!("{}", response_text);
-	}
+        let response_text = response.text().await?;
+        panic!("{}", response_text);
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 struct ChatMessage {
-	role: String,
-	content: String,
+    role: String,
+    content: String,
 }
-
--- src/file/mdfile/mod.rs
+++ src/file/mdfile/mod.rs
@@ -1,207 +1,222 @@
-use serde::{Deserialize, Serialize};
 use regex::Regex;
+use serde::{Deserialize, Serialize};
 
-// use crate::prelude::*;
+use crate::prelude::*;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct MDFile {
-	yaml: Option<serde_yaml::Value>,
-	body: String,
-	embedding: Option<Vec<f32>>,
+    yaml: Option<serde_yaml::Value>,
+    body: String,
+    embedding: Option<Vec<f64>>,
 }
 
 impl MDFile {
-	// Constructor
-	pub fn new(yaml: Option<serde_yaml::Value>, body: String) -> Self {
-		Self {
-			yaml,
-			body,
-			embedding: None,
-		}
-	}
+    // Constructor
+    pub fn new(yaml: Option<serde_yaml::Value>, body: String) -> Self {
+        Self {
+            yaml,
+            body,
+            embedding: None,
+        }
+    }
 
-	// Setter / Getter Methods
-	pub fn set_yaml(&mut self, yaml: serde_yaml::Value) {
-		if self.yaml.as_ref() != Some(&yaml) {
-			self.embedding = None;
-		}
-		self.yaml = Some(yaml);
-	}
-	pub fn add_yaml_key(&mut self, key: String, value: serde_yaml::Value) {
-		self.embedding = None;
-		if let Some(yaml) = &mut self.yaml {
-			if let serde_yaml::Value::Mapping(mapping) = yaml {
-				mapping.insert(serde_yaml::Value::String(key), value);
-			}
-		} else {
-			let mut new_yaml: serde_yaml::Mapping = serde_yaml::Mapping::new();
-			new_yaml.insert(serde_yaml::Value::String(key), value);
-			self.yaml = Some(serde_yaml::Value::Mapping(new_yaml));
-		}
-	}
-	pub fn get_yaml(&self) -> Option<&serde_yaml::Value> {
-		self.yaml.as_ref()
-	}
-	pub fn get_yaml_key(&self, key: &str) -> Option<&serde_yaml::Value> {
-		if let Some(yaml) = &self.yaml {
-			if let serde_yaml::Value::Mapping(mapping) = yaml {
-				mapping.get(&serde_yaml::Value::String(key.to_string()))
-			} else {
-				None
-			}
-		} else {
-			None
-		}
-	}
+    // Setter / Getter Methods
+    pub fn set_yaml(&mut self, yaml: serde_yaml::Value) {
+        if self.yaml.as_ref() != Some(&yaml) {
+            self.embedding = None;
+        }
+        self.yaml = Some(yaml);
+    }
+    pub fn add_yaml_key(&mut self, key: String, value: serde_yaml::Value) {
+        self.embedding = None;
+        if let Some(yaml) = &mut self.yaml {
+            if let serde_yaml::Value::Mapping(mapping) = yaml {
+                mapping.insert(serde_yaml::Value::String(key), value);
+            }
+        } else {
+            let mut new_yaml: serde_yaml::Mapping = serde_yaml::Mapping::new();
+            new_yaml.insert(serde_yaml::Value::String(key), value);
+            self.yaml = Some(serde_yaml::Value::Mapping(new_yaml));
+        }
+    }
+    pub fn get_yaml(&self) -> Option<&serde_yaml::Value> {
+        self.yaml.as_ref()
+    }
+    pub fn get_yaml_key(&self, key: &str) -> Option<&serde_yaml::Value> {
+        if let Some(yaml) = &self.yaml {
+            if let serde_yaml::Value::Mapping(mapping) = yaml {
+                mapping.get(&serde_yaml::Value::String(key.to_string()))
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
 
-	pub fn set_body(&mut self, body: String) {
-		self.embedding = None;
-		self.body = body;
-	}
-	pub fn get_body(&self) -> &String {
-		&self.body
-	}
+    pub fn set_body(&mut self, body: String) {
+        self.embedding = None;
+        self.body = body;
+    }
+    pub fn get_body(&self) -> &String {
+        &self.body
+    }
 
-	// Serialization Methods
-	pub fn from_string(contents: String) -> Self {
-		let yaml_pattern = Regex::new(r"^(\-\-\-\n(?P<yaml>[\s\S]*?)\n?\-\-\-\n?)?(?P<body>[\s\S]*)").unwrap();
-		let captures = yaml_pattern.captures(&contents).unwrap();
-		let yaml = captures.name("yaml").map(|m| serde_yaml::from_str(m.as_str()).unwrap());
-		let body = captures.name("body").unwrap().as_str().to_string();
-		Self::new(yaml, body)
-	}
-	pub fn to_string(&self) -> String {
-		if let Some(yaml) = &self.yaml {
-			let yaml_str = serde_yaml::to_string(yaml).unwrap();
-			format!("---\n{}---\n{}", yaml_str, self.body)
-		} else {
-			self.body.clone()
-		}
-	}
+    // Serialization Methods
+    pub fn from_string(contents: String) -> Self {
+        let yaml_pattern =
+            Regex::new(r"^(\-\-\-\n(?P<yaml>[\s\S]*?)\n?\-\-\-\n?)?(?P<body>[\s\S]*)").unwrap();
+        let captures = yaml_pattern.captures(&contents).unwrap();
+        let yaml = captures
+            .name("yaml")
+            .map(|m| serde_yaml::from_str(m.as_str()).unwrap());
+        let body = captures.name("body").unwrap().as_str().to_string();
+        Self::new(yaml, body)
+    }
+    pub fn to_string(&self) -> String {
+        if let Some(yaml) = &self.yaml {
+            let yaml_str = serde_yaml::to_string(yaml).unwrap();
+            format!("---\n{}---\n{}", yaml_str, self.body)
+        } else {
+            self.body.clone()
+        }
+    }
 
-	// Embedding Methods
-	pub fn create_embedding(&self) -> Vec<f32> {
-		todo!()
-	}
-	pub fn update_embedding(&mut self) {
-		todo!()
-	}
-	pub fn get_embedding(&self) -> Vec<f32> {
-		todo!()
-	}
+    // Embedding Methods
+    pub async fn update_embedding(&mut self, driver: &crate::ai::api::AIDriver) -> Result<()> {
+        self.embedding = Some(driver.get_embedding(&self.to_string()).await?);
+        Ok(())
+    }
+    pub fn get_embedding(&self) -> Option<&Vec<f64>> {
+        self.embedding.as_ref()
+    }
 }
 
 #[cfg(test)]
 mod mdfile_tests {
 
-	use super::*;
+    use super::*;
 
-	// Setter / Getter Tests
-	#[test]
-	fn test_get_set_yaml() {
-		let mut mdfile = MDFile::new(None, "# Test\n\nThis is a test file.".to_string());
-		let mut yaml = serde_yaml::Value::Mapping(serde_yaml::Mapping::new());
-		yaml.as_mapping_mut().unwrap().insert(serde_yaml::Value::String("key".to_string()), serde_yaml::Value::String("value".to_string()));
-		mdfile.set_yaml(yaml.clone());
+    // Setter / Getter Tests
+    #[test]
+    fn test_get_set_yaml() {
+        let mut mdfile = MDFile::new(None, "# Test\n\nThis is a test file.".to_string());
+        let mut yaml = serde_yaml::Value::Mapping(serde_yaml::Mapping::new());
+        yaml.as_mapping_mut().unwrap().insert(
+            serde_yaml::Value::String("key".to_string()),
+            serde_yaml::Value::String("value".to_string()),
+        );
+        mdfile.set_yaml(yaml.clone());
 
-		let expected = Some(&yaml);
-		let actual = mdfile.get_yaml();
-		assert_eq!(actual, expected);
-	}
-	#[test]
-	fn test_add_yaml_key() {
-		let mut mdfile = MDFile::new(None, "# Test\n\nThis is a test file.".to_string());
-		mdfile.add_yaml_key("key".to_string(), serde_yaml::Value::String("value".to_string()));
+        let expected = Some(&yaml);
+        let actual = mdfile.get_yaml();
+        assert_eq!(actual, expected);
+    }
+    #[test]
+    fn test_add_yaml_key() {
+        let mut mdfile = MDFile::new(None, "# Test\n\nThis is a test file.".to_string());
+        mdfile.add_yaml_key(
+            "key".to_string(),
+            serde_yaml::Value::String("value".to_string()),
+        );
 
-		let binding = serde_yaml::Value::String("value".to_string());
-  		let expected = Some(&binding);
-		let actual = mdfile.get_yaml_key("key");
-		assert_eq!(actual, expected);
-	}
-	#[test]
-	fn test_get_yaml_key() {
-		let mut mdfile = MDFile::new(None, "# Test\n\nThis is a test file.".to_string());
-		let mut yaml = serde_yaml::Value::Mapping(serde_yaml::Mapping::new());
-		yaml.as_mapping_mut().unwrap().insert(serde_yaml::Value::String("key".to_string()), serde_yaml::Value::String("value".to_string()));
-		mdfile.set_yaml(yaml);
+        let binding = serde_yaml::Value::String("value".to_string());
+        let expected = Some(&binding);
+        let actual = mdfile.get_yaml_key("key");
+        assert_eq!(actual, expected);
+    }
+    #[test]
+    fn test_get_yaml_key() {
+        let mut mdfile = MDFile::new(None, "# Test\n\nThis is a test file.".to_string());
+        let mut yaml = serde_yaml::Value::Mapping(serde_yaml::Mapping::new());
+        yaml.as_mapping_mut().unwrap().insert(
+            serde_yaml::Value::String("key".to_string()),
+            serde_yaml::Value::String("value".to_string()),
+        );
+        mdfile.set_yaml(yaml);
 
-		let binding = serde_yaml::Value::String("value".to_string());
-  		let expected = Some(&binding);
-		let actual = mdfile.get_yaml_key("key");
-		assert_eq!(actual, expected);
-	}
-	#[test]
-	fn test_get_set_body() {
-		let mut mdfile = MDFile::new(None, "# Test\n\nThis is a test file.".to_string());
-		mdfile.set_body("# New Test\n\nThis is a new test file.".to_string());
+        let binding = serde_yaml::Value::String("value".to_string());
+        let expected = Some(&binding);
+        let actual = mdfile.get_yaml_key("key");
+        assert_eq!(actual, expected);
+    }
+    #[test]
+    fn test_get_set_body() {
+        let mut mdfile = MDFile::new(None, "# Test\n\nThis is a test file.".to_string());
+        mdfile.set_body("# New Test\n\nThis is a new test file.".to_string());
 
-		let expected = "# New Test\n\nThis is a new test file.".to_string();
-		let actual = mdfile.get_body();
-		assert_eq!(actual, &expected);
-	}
+        let expected = "# New Test\n\nThis is a new test file.".to_string();
+        let actual = mdfile.get_body();
+        assert_eq!(actual, &expected);
+    }
 
+    // Serialization Tests
+    #[test]
+    fn test_from_string_with_yaml() {
+        let contents = "---\nkey: value\n---\n# Test\n\nThis is a test file.".to_string();
 
-	// Serialization Tests
-	#[test]
-	fn test_from_string_with_yaml() {
-		let contents = "---\nkey: value\n---\n# Test\n\nThis is a test file.".to_string();
+        let actual = MDFile::from_string(contents);
+        let mut yaml_expected: serde_yaml::Mapping = serde_yaml::Mapping::new();
+        yaml_expected.insert(
+            serde_yaml::Value::String("key".to_string()),
+            serde_yaml::Value::String("value".to_string()),
+        );
+        let expected = MDFile {
+            yaml: Some(serde_yaml::Value::Mapping(yaml_expected)),
+            body: "# Test\n\nThis is a test file.".to_string(),
+            embedding: None,
+        };
+        assert_eq!(actual, expected);
+    }
 
-		let actual = MDFile::from_string(contents);
-		let mut yaml_expected: serde_yaml::Mapping = serde_yaml::Mapping::new();
-		yaml_expected.insert(serde_yaml::Value::String("key".to_string()), serde_yaml::Value::String("value".to_string()));
-		let expected = MDFile {
-			yaml: Some(serde_yaml::Value::Mapping(yaml_expected)),
-			body: "# Test\n\nThis is a test file.".to_string(),
-			embedding: None,
-		};
-		assert_eq!(actual, expected);
-	}
-
-	#[test]
-	fn test_to_string_with_yaml() {
-		let mut yaml_expected: serde_yaml::Mapping = serde_yaml::Mapping::new();
-		yaml_expected.insert(serde_yaml::Value::String("key".to_string()), serde_yaml::Value::String("value".to_string()));
-		let mdfile = MDFile {
-			yaml: Some(serde_yaml::Value::Mapping(yaml_expected)),
-			body: "# Test\n\nThis is a test file.".to_string(),
-			embedding: None,
-		};
-		let actual = mdfile.to_string();
-		let expected = r#"---
+    #[test]
+    fn test_to_string_with_yaml() {
+        let mut yaml_expected: serde_yaml::Mapping = serde_yaml::Mapping::new();
+        yaml_expected.insert(
+            serde_yaml::Value::String("key".to_string()),
+            serde_yaml::Value::String("value".to_string()),
+        );
+        let mdfile = MDFile {
+            yaml: Some(serde_yaml::Value::Mapping(yaml_expected)),
+            body: "# Test\n\nThis is a test file.".to_string(),
+            embedding: None,
+        };
+        let actual = mdfile.to_string();
+        let expected = r#"---
 key: value
 ---
 # Test
 
-This is a test file."#.to_string();
-		assert_eq!(actual, expected);
-	}
+This is a test file."#
+            .to_string();
+        assert_eq!(actual, expected);
+    }
 
-	#[test]
-	fn test_from_string_without_yaml() {
-		let contents = "# Test\n\nThis is a test file.".to_string();
+    #[test]
+    fn test_from_string_without_yaml() {
+        let contents = "# Test\n\nThis is a test file.".to_string();
 
-		let actual = MDFile::from_string(contents);
-		let expected = MDFile {
-			yaml: None,
-			body: "# Test\n\nThis is a test file.".to_string(),
-			embedding: None,
-		};
-		assert_eq!(actual, expected);
-	}
+        let actual = MDFile::from_string(contents);
+        let expected = MDFile {
+            yaml: None,
+            body: "# Test\n\nThis is a test file.".to_string(),
+            embedding: None,
+        };
+        assert_eq!(actual, expected);
+    }
 
-	#[test]
-	fn test_to_string_without_yaml() {
-		let mdfile = MDFile {
-			yaml: None,
-			body: "# Test\n\nThis is a test file.".to_string(),
-			embedding: None,
-		};
-		let actual = mdfile.to_string();
-		let expected = r#"# Test
+    #[test]
+    fn test_to_string_without_yaml() {
+        let mdfile = MDFile {
+            yaml: None,
+            body: "# Test\n\nThis is a test file.".to_string(),
+            embedding: None,
+        };
+        let actual = mdfile.to_string();
+        let expected = r#"# Test
 
-This is a test file."#.to_string();
-		assert_eq!(actual, expected);
-	}
-
-
+This is a test file."#
+            .to_string();
+        assert_eq!(actual, expected);
+    }
 }
